24932240995c3fcdc98b5c9351e923cfccabc9d8
ForkEyeee <shawncarter123456@gmail.com>
2024-01-13 18:23:21 -0500

    adjusted environment variables

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,12 +1,11 @@
-import { socket } from '@/lib/socket';
+// import { socket } from '@/lib/socket';
 
 export default async function getRoomInfo() {
-  let room;
-  socket.emit('find_rooms');
-  socket.on('room_info', roomInfo => {
-    room = roomInfo;
-    return room;
-  });
-
-  return room;
+  // let room;
+  // socket.emit('find_rooms');
+  // socket.on('room_info', roomInfo => {
+  //   room = roomInfo;
+  //   return room;
+  // });
+  // return room;
 }
