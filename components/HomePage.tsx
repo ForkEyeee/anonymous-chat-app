12569a0c5c0cc81e2e6000bc1a99a07ed9019647
ForkEyeee <shawncarter123456@gmail.com>
2024-01-13 19:41:24 -0500

    debugging prod

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -7,19 +7,25 @@ import ChatBox from './ChatBox';
 import MessageList from './MessageList';
 import UserInformation from './UserInformation';
 
-socket.on('connect', () => {
-  console.log(socket.id);
-});
-
 const HomePage = () => {
   const [room, setRoom] = useState({});
+  const [socketId, setSocketId] = useState('');
 
   useEffect(() => {
+    const handleConnect = () => {
+      console.log(socket.id);
+      setSocketId(socket.id);
+    };
+
+    socket.on('connect', handleConnect);
+
     socket.emit('find_room');
     socket.on('room_info', roomInfo => {
       setRoom(roomInfo);
     });
+
     return () => {
+      socket.off('connect', handleConnect);
       socket.off('find_room');
       socket.off('room_info');
     };
@@ -30,11 +36,9 @@ const HomePage = () => {
   return (
     <div>
       <UserInformation />
-
       <div>Room: {room.roomID}</div>
-      <h1>Current User: {socket.id}</h1>
+      <h1>Current User: {socketId}</h1>
       <h1>Online: {room.size}</h1>
-
       <MessageList socket={socket} />
       <ChatBox socket={socket} />
     </div>
